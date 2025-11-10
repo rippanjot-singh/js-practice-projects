3633054b741031a2b28e5b4ec9f23e80e2673de1
Rippanjot Singh <rippanjotsingh90@gmail.com>
2025-11-10 16:57:26 +0530

    time-travel-3

--- time-travel/script.js
+++ time-travel/script.js
@@ -10,28 +10,35 @@ let topdiv = document.querySelector('.top');
 function setdate() {
     present.value = `${currentdate.getFullYear()}-${currentdate.getMonth() + 1}-${currentdate.getDate()}`;
 }
-function call() {
 
-}
-
+setdate();
 
 btn.addEventListener('click', () => {
-    p.style.display = 'block';
-    let calls = ['Calling API', 'Call Rejected', 'Calling NASA', 'Call Rejected', 'NASA Gai ***', 'Data Fetched'];
+    if (future.value > '2025-12-31' && future.value != '') {
+        console.log('hello', future.value);
+        p.style.display = 'block';
+        let calls = ['Calling API', 'Call Rejected', 'Calling NASA', 'Call Rejected', 'NASA Khaye Tatti', 'Data Fetched'];
 
-    btn.style.display = 'none';
-    fields.forEach((field) => {
-        field.style.display = 'none';
-    })
-    let i = 0;
+        btn.style.display = 'none';
+        fields.forEach((field) => {
+            field.style.display = 'none';
+        })
+        let i = 0;
         setInterval(() => {
             i++;
             p.innerText = calls[i];
         }, 2000);
-    setTimeout(() => {
-        p.style.display = 'none';
-        topdiv.innerHTML += '<video src="video/26-me-to-dunya-khatam-hai.mp4" autoplay></video>';
-    }, 2000 * calls.length)
-})
+        setTimeout(() => {
+            p.style.display = 'none';
+            topdiv.innerHTML += '<video src="video/26-me-to-dunya-khatam-hai.mp4" autoplay></video>';
+        }, 2000 * calls.length)
+    } else {
+        //Make the alert color red
 
-setdate();
+        alert('The Future Date Should Not Be The Same As The Current Year Or Earlier or Blank');
+    }
+})
+console.log(window);
+
+
+
