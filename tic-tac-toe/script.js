ae8b3947ede29fe2dc9c5b02c0867df4a379f986
Rippanjot Singh <rippanjotsingh90@gmail.com>
2025-11-12 12:29:31 +0530

    tic-tac-toe-v-02

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -3,6 +3,7 @@ let sm = document.querySelector("#statusMessage");
 let restart = document.querySelector("#restartButton");
 let board = document.querySelector("#gameBoard");
 h1 = document.querySelector("h1");
+let gameOver = false;
 let X = "X";
 let O = "O";
 let turn = 0;
@@ -18,14 +19,17 @@ function win(){
             [1, 5, 9],
             [3, 5, 7]
         ]
+
     for (let i = 0; i < chances.length; i++) {
             if (O == cell[chances[i][0] - 1].innerHTML && O == cell[chances[i][1] - 1].innerHTML && O == cell[chances[i][2] - 1].innerHTML) {
                 h1.innerHTML = "O won!!!";
                 sm.innerHTML = "";
+                gameOver = true;
             }
             else if (X == cell[chances[i][0] - 1].innerHTML && X == cell[chances[i][1] - 1].innerHTML && X == cell[chances[i][2] - 1].innerHTML) {
                 h1.innerHTML = "X won!!!";
                 sm.innerHTML = "";
+                gameOver = true;
             }
         }
 }
@@ -45,9 +49,10 @@ function draw(){
 
 for (let i = 0; i < cell.length; i++) {
     cell[i].addEventListener('click', () => {
-        console.log('hi tanya');
-        
         turn++
+
+        if (gameOver) return;
+
         if (cell[i].innerHTML == "") {
             if (turn % 2 == 0) {
                 cell[i].innerHTML = X;
@@ -70,6 +75,7 @@ function restartfnc(){
         cell.innerHTML = "";
         h1.innerHTML = "Tic Tac Toe";
         sm.innerHTML = "X's Turn";
+        gameOver = false;
     })
 }
 
